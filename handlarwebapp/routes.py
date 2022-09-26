5055dbecd43b95157b3a6ccafa3e1017d2b53bec
August Aublet <august.aublet@gmail.com>
2022-09-26 15:12:43 +0200

    lagt till item put. updatera itemName och itemIsDone

--- handlarwebapp/routes.py
+++ handlarwebapp/routes.py
@@ -1,21 +1,22 @@
-from importlib.metadata import requires
 from flask_restful import Resource, abort, reqparse
 from handlarwebapp.models import Shoppinglist, Listitem, listItemsInJson, listTitleInJson
 from handlarwebapp import db
-import json
-from flask import jsonify
 
-SECRETKEY = "mklsUgFnJ2AaFGeWz-RjjQ"
 
-def abort_if_list_id_does_not_exist(list_id):
-    exists = db.session.query(Shoppinglist.id).filter_by(id=list_id).first() is not None
-    if exists == None:
-        abort(404, message="Shoppinglist with id {} doesn't exist".format(list_id))
+# SECRETKEY = "mklsUgFnJ2AaFGeWz-RjjQ"
+
+# def abort_if_list_id_does_not_exist(list_id):
+#     exists = db.session.query(Shoppinglist.id).filter_by(id=list_id).first() is not None
+#     if exists == None:
+#         abort(404, message="Shoppinglist with id {} doesn't exist".format(list_id))
 
 list_parser = reqparse.RequestParser()
-item_parser = reqparse.RequestParser()
-list_parser.add_argument("newListTitle", required=True, help= "ListTitle kan inte vara blank")
-item_parser.add_argument("newItemName",  required=True, help="itemName kan inte vara blank")
+new_item_parser = reqparse.RequestParser()
+update_item_parser = reqparse.RequestParser()
+list_parser.add_argument("newListTitle", required=True, help= "newListTitle kan inte vara blank")
+new_item_parser.add_argument("newItemName",  required=True, help="newItemName kan inte vara blank")
+update_item_parser.add_argument("newItemName", required=True, help="newItemName kan inte vara blank")
+update_item_parser.add_argument("itemIsDone", type=bool, )
 
 class ShoppingListsResorce(Resource):
     # returnerar alla listor i json (listtitle, id)
@@ -42,14 +43,17 @@ class ListItemResorce(Resource):
         return listItemsInJson(list_id)
 
     def post(self, list_id):
-        args = item_parser.parse_args()
+        args = new_item_parser.parse_args()
         db.session.add(Listitem(itemName=args["newItemName"], shoppinglist_id=list_id))
         db.session.commit()
         return listItemsInJson(list_id)
     
     def put(self,list_id,item_id):
-        
-        pass
+        args = update_item_parser.parse_args()
+        itemToUpdate = Listitem.query.get(item_id)
+        itemToUpdate.itemName = args["newItemName"]
+        itemToUpdate.itemIsDone = args["itemIsDone"]
+        return listItemsInJson(list_id)
 
     def delete(self,list_id,item_id):
         db.session.delete(Listitem.query.get(item_id))
