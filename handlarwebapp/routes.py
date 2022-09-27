f90c9a3c81c5a8a9190077302cc09f41ab57381a
August Aublet <august.aublet@gmail.com>
2022-09-27 11:59:10 +0200

    Lagt till deleteAllDone tagit bort new_item_parcer och använder istället item_parser

--- handlarwebapp/routes.py
+++ handlarwebapp/routes.py
@@ -12,11 +12,12 @@ from handlarwebapp import db
 
 list_parser = reqparse.RequestParser()
 new_item_parser = reqparse.RequestParser()
-update_item_parser = reqparse.RequestParser()
+item_parser = reqparse.RequestParser()
+
 list_parser.add_argument("newListTitle", required=True, help= "newListTitle kan inte vara blank")
-new_item_parser.add_argument("newItemName",  required=True, help="newItemName kan inte vara blank")
-update_item_parser.add_argument("newItemName", required=True, help="newItemName kan inte vara blank")
-update_item_parser.add_argument("itemIsDone", type=bool, )
+item_parser.add_argument("newItemName")
+item_parser.add_argument("itemIsDone", type=bool)
+item_parser.add_argument("deleteAllDone", type= bool, help= "this command neads to be a bool")
 
 class ShoppingListsResorce(Resource):
     # returnerar alla listor i json (listtitle, id)
@@ -31,7 +32,7 @@ class ShoppingListsResorce(Resource):
         db.session.commit()
 
         return listTitleInJson()  
-
+    # tar bort en lista men hjälp av list_id
     def delete(self, list_id):
         db.session.delete(Shoppinglist.query.get(list_id))
         db.session.commit()
@@ -39,22 +40,33 @@ class ShoppingListsResorce(Resource):
 
 
 class ListItemResorce(Resource):
+    # returnerar en lista efter list_id i json itemName, id, itemIsDone
     def get(self,list_id):
         return listItemsInJson(list_id)
-
+    # lägga till item i listan(kräver newItemName) och 
+    # ta bort alla avklarade(itemIsDone) med deleteAllDone=true
     def post(self, list_id):
-        args = new_item_parser.parse_args()
-        db.session.add(Listitem(itemName=args["newItemName"], shoppinglist_id=list_id))
-        db.session.commit()
+        args = item_parser.parse_args()
+        if args.deleteAllDone == True:          # kollar om deleteAllDone existerar
+            Listitem.query.filter_by(shoppinglist_id=list_id, itemIsDone = True).delete()
+            db.session.commit()
+            return listItemsInJson(list_id)
+        else:
+            db.session.add(Listitem(itemName=args["newItemName"], shoppinglist_id=list_id))
+            db.session.commit()
         return listItemsInJson(list_id)
     
+    #uppdaterar item med newItemName och itemIsDone
     def put(self,list_id,item_id):
-        args = update_item_parser.parse_args()
+        args = item_parser.parse_args()
         itemToUpdate = Listitem.query.get(item_id)
         itemToUpdate.itemName = args["newItemName"]
         itemToUpdate.itemIsDone = args["itemIsDone"]
+        db.session.commit()
         return listItemsInJson(list_id)
 
+    # tar bort ett item efte item_id. list_id krävs endast för att 
+    # returnera rätt lista(också lättare med path)
     def delete(self,list_id,item_id):
         db.session.delete(Listitem.query.get(item_id))
         db.session.commit()
